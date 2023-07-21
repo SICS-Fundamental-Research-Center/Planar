604f5d07db7c0eb3bdc5823eb1ab580a332a2b11
Shuhao Liu <liushuhaothu@gmail>
2023-07-21 12:20:25 +0800

    Added data type definition file.

---
+++ core/common/types.h
@@ -0,0 +1,17 @@
+//
+// Created by Shuhao Liu on 2023-07-18.
+//
+
+#ifndef GRAPH_SYSTEMS_TYPES_H
+#define GRAPH_SYSTEMS_TYPES_H
+
+#include <cstdint>
+
+namespace sics::graph::core::common {
+
+typedef uint32_t GraphIDType;
+
+} // namespace sics::graph::core::common
+
+
+#endif  // GRAPH_SYSTEMS_TYPES_H
