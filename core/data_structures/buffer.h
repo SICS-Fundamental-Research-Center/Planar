0c350d5f22eac134ca77b606349e4a1d211f4c00
bai-wenchao <wenchao.bai.chn@gmail.com>
2023-07-24 11:27:05 +0000

    completed rule of five

--- core/data_structures/buffer.h
+++ core/data_structures/buffer.h
@@ -1,44 +1,37 @@
 #ifndef CORE_DATA_STRUCTURES_BUFFER_H_
 #define CORE_DATA_STRUCTURES_BUFFER_H_
 
-#include <cstdint>
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
-
 
 namespace sics::graph::core::data_structures {
 
 class Buffer {
  public:
-  explicit Buffer(uint8_t* p,  size_t s) : p_(p), s_(s) {}
+  explicit Buffer(uint8_t* p, size_t s) : p_(p), s_(s) {}
   virtual ~Buffer() = default;
 
-  uint8_t* Get(size_t offset = 0) {
-    return p_ + offset;
-  }
+  uint8_t* Get(size_t offset = 0) { return p_ + offset; }
 
-  size_t GetSize() const {
-    return s_;
-  }
+  size_t GetSize() const { return s_; }
 
-  void SetPointer(uint8_t* p) {
-    p_ = p;
-  }
+  void SetPointer(uint8_t* p) { p_ = p; }
 
  protected:
   uint8_t* p_;
-  size_t  s_;
+  size_t s_;
 };
-
 
 class OwnedBuffer : public Buffer {
  public:
-  explicit OwnedBuffer(size_t s) : Buffer(reinterpret_cast<uint8_t*>(malloc(s)), s) {}
+  explicit OwnedBuffer(size_t s)
+      : Buffer(static_cast<uint8_t*>(malloc(s)), s) {}
   OwnedBuffer(const OwnedBuffer& r) = delete;
+  OwnedBuffer& operator=(const OwnedBuffer& r) = delete;
   OwnedBuffer(OwnedBuffer&& r) = default;
-  ~OwnedBuffer() override {
-    delete p_;
-  }
+  OwnedBuffer& operator=(OwnedBuffer&& r) = default;
+  ~OwnedBuffer() override { delete p_; }
 
   Buffer GetReference(size_t offset, size_t s);
 };
@@ -46,4 +39,3 @@ class OwnedBuffer : public Buffer {
 }  // namespace sics::graph::core::data_structures
 
 #endif  // CORE_DATA_STRUCTURES_BUFFER_H_
-
