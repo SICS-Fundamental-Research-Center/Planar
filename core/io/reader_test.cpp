e0baa4bd580ec60024c5863faa8506591aa8c0c1
bai-wenchao <wenchao.bai.chn@gmail.com>
2023-07-24 08:22:13 +0000

    wrote test demo.

--- core/io/reader_test.cpp
+++ core/io/reader_test.cpp
@@ -14,15 +14,18 @@ class ReaderTest : public ::testing::Test {
 TEST_F(ReaderTest, JudgeAdaptTest) {
   // Test the JudgeAdapt function of the Reader class
   // Instantiate a Reader object and call the JudgeAdapt function
+
+  // EXPECT_STRNE("hello", "world");
+
   sics::graph::core::io::Reader reader(
       "/home/baiwc/workspace/graph-systems/input/minigraph_si/0.yaml");
   bool result = reader.JudgeAdapt();
   ASSERT_TRUE(result);
 }
 
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+// int main(int argc, char** argv) {
+//   ::testing::InitGoogleTest(&argc, argv);
+//   return RUN_ALL_TESTS();
+// }
 
 }  // namespace sics::graph::core::test
