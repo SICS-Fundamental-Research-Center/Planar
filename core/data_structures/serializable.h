b69761f0476044a39d23b30f58ec1f6ddf7380e0
bai-wenchao <wenchao.bai.chn@gmail.com>
2023-07-26 12:52:06 +0000

    add comments to key functions.

--- core/data_structures/serializable.h
+++ core/data_structures/serializable.h
@@ -8,9 +8,18 @@
 
 namespace sics::graph::core::data_structures {
 
+// This class defines the Serializable interface, which is used to serialize and
+// deserialize objects. The Serializable interface provides methods for
+// serializing an object to a Serialized object and deserializing an object from
+// a Serialized object.
 class Serializable {
  public:
+  // Serialize the `Serializable` object to a `Serialized` object.
+  // This function will submit the serialization task to the given TaskRunner
   virtual std::unique_ptr<Serialized> Serialize(common::TaskRunner& runner) = 0;
+
+  // Deserialize the `Serializable` object from a `Serialized` object.
+  // This function will submit the deserialization task to the given TaskRunner
   virtual void Deserialize(common::TaskRunner& runner,
                            Serialized&& serialized) = 0;
 };
--- core/data_structures/serialized.h
+++ core/data_structures/serialized.h
@@ -9,10 +9,35 @@ namespace sics::graph::core::data_structures {
 
 class Serialized {
  public:
+  // `HasNext()` is called when `Writer` writes `Serialized` class back to disk.
+  //
+  // e.g. in `Writer::Write()`:
+  //  while (serialized.HasNext) {
+  //    std::list<OwnedBuffer> buffers = serialized.PopNext();
+  //
+  //    /* Write buffers to disks */
+  //
+  //  }
   virtual bool HasNext() const = 0;
 
+  // `ReceiveBuffers()` is called when `Reader` reads `Serialized` from disk.
+  //
+  // The reason to use `std::list<OwnedBuffer>` instead of `OwnedBuffer` is to
+  // make it compatible with parallel reading in which case multiple
+  // `OwnedBuffer`s are read from the same data file at the same time.
+  //
+  // `SetComplete()` should be called when all buffers are received.
+  //
+  // e.g. in `Reader::ReadBinFile(const std::string& path, Serialized* dst)`:
+  //  while (/* folder.HasUnreadFile() */) {
+  //    std::list<OwnedBuffer> file_buffers;
+  //    /* Read data from disk to file_buffers */
+  //    dst->ReceiveBuffers(std::move(file_buffers));
+  //  }
   virtual void ReceiveBuffers(std::list<OwnedBuffer>&& buffers) = 0;
-
+  
+  // `PopNext()` is called when `Writer` writes `Serialized` class back to disk.
+  // It returns a list of `OwnedBuffer`s that are ready to be written to disk.
   std::list<OwnedBuffer> PopNext() {
     is_complete_ = false;
     return PopNextImpl();
