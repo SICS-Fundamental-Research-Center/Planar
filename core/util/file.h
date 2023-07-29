726431b81a59b23278340f141f10f2533826d767
hsiaoko <zhuxk@buaa.edu.cn>
2023-07-29 09:17:10 +0000

    Revised file.h and fixed some issues in previous PR.

--- core/util/file.h
+++ core/util/file.h
@@ -1,8 +1,8 @@
 #ifndef CORE_UTIL_FILE_H_
 #define CORE_UTIL_FILE_H_
 
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <filesystem>
+#include <fstream>
 
 namespace sics::graph::core::util {
 namespace file {
@@ -11,26 +11,13 @@ namespace file {
 //
 // Function MakeDirectory() create the DIRECTORY(path), if they do not already
 // "Exist()".
-void MakeDirectory(const std::string& path) {
-  std::string dir = path;
-  int len = dir.size();
-  if (dir[len - 1] != '/') {
-    dir[len] = '/';
-    len++;
-  }
-  std::string temp;
-  for (int i = 1; i < len; i++) {
-    if (dir[i] == '/') {
-      temp = dir.substr(0, i);
-      if (access(temp.c_str(), 0) != 0)
-        if (mkdir(temp.c_str(), 0777) != 0) VLOG(1) << "failed operaiton.";
-    }
-  }
+inline void MakeDirectory(const std::string& path) {
+  std::filesystem::create_directory(path);
 }
 
-bool Exist(const std::string& path) {
-  struct stat buffer;
-  return (stat(path.c_str(), &buffer) == 0);
+inline bool Exist(const std::string& path) {
+  std::ifstream f(path.c_str());
+  return f.good();
 }
 
 }  // namespace file
