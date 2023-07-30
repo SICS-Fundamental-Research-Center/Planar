be8442b5a2eac146c701998faf4f29fa724eef98
hsiaoko <zhuxk@buaa.edu.cn>
2023-07-30 07:55:20 +0000

    Implement ThreadPool::SubmitSync().

--- core/common/multithreading/thread_pool.cpp
+++ core/common/multithreading/thread_pool.cpp
@@ -3,13 +3,13 @@
 namespace sics::graph::core::common {
 
 ThreadPool::ThreadPool(uint32_t num_threads)
-    : internal_pool_((unsigned int) num_threads) {}
+    : internal_pool_((unsigned int)num_threads) {}
 
 void ThreadPool::SubmitAsync(Task&& task) {
   internal_pool_.add(std::move(task));
 }
 
-void ThreadPool::SubmitAsync(Task&& task, std::function<void ()> callback) {
+void ThreadPool::SubmitAsync(Task&& task, std::function<void()> callback) {
   internal_pool_.add(std::move(task));
   // TODO: Implement the call back behavior.
 }
@@ -21,7 +21,7 @@ void ThreadPool::SubmitAsync(const TaskPackage& tasks) {
 }
 
 void ThreadPool::SubmitAsync(const TaskPackage& tasks,
-                             std::function<void ()> callback) {
+                             std::function<void()> callback) {
   for (const auto& t : tasks) {
     internal_pool_.add(t);
   }
@@ -34,10 +34,17 @@ void ThreadPool::SubmitSync(Task&& task) {
 }
 
 void ThreadPool::SubmitSync(const TaskPackage& tasks) {
+  std::mutex mtx;
+  std::condition_variable finish_cv;
+  std::unique_lock<std::mutex> lck(mtx);
+  std::atomic<size_t> pending_packages(internal_pool_.numThreads());
   for (const auto& t : tasks) {
-    internal_pool_.add(t);
+    internal_pool_.add([this, &finish_cv, &pending_packages, &t]() {
+      t();
+      if (pending_packages.fetch_sub(1) == 1) finish_cv.notify_all();
+    });
   }
-  // TODO: Implement the blocking behavior.
+  finish_cv.wait(lck, [&] { return pending_packages.load() == 0; });
 }
 
 size_t ThreadPool::GetParallelism() const {
