139d5abb798457479264eacbe34f6c4af5047699
hsiaoko <zhuxk@buaa.edu.cn>
2023-07-31 03:30:17 +0000

    Fixed an error in ThreadPool.SubmitSync() and added its corresponding test file.

--- core/common/multithreading/thread_pool.cpp
+++ core/common/multithreading/thread_pool.cpp
@@ -34,17 +34,24 @@ void ThreadPool::SubmitSync(Task&& task) {
 }
 
 void ThreadPool::SubmitSync(const TaskPackage& tasks) {
+  auto parallelism = internal_pool_.numThreads();
   std::mutex mtx;
   std::condition_variable finish_cv;
   std::unique_lock<std::mutex> lck(mtx);
-  std::atomic<size_t> pending_packages(internal_pool_.numThreads());
-  for (const auto& t : tasks) {
-    internal_pool_.add([this, &finish_cv, &pending_packages, &t]() {
-      t();
-      if (pending_packages.fetch_sub(1) == 1) finish_cv.notify_all();
-    });
+  std::atomic<size_t> pending_threads(parallelism);
+
+  for (size_t tid = 0; tid < parallelism; tid++) {
+    internal_pool_.add(
+        [this, tid, &finish_cv, &pending_threads, &tasks, &parallelism]() {
+          for (size_t i = tid; i < tasks.size(); i += parallelism) {
+            LOG_INFO(i, "/", tasks.size(), "/", parallelism);
+            auto task = tasks.at(i);
+            task();
+          }
+          if (pending_threads.fetch_sub(1) == 1) finish_cv.notify_all();
+        });
   }
-  finish_cv.wait(lck, [&] { return pending_packages.load() == 0; });
+  finish_cv.wait(lck, [&] { return pending_threads.load() == 0; });
 }
 
 size_t ThreadPool::GetParallelism() const {
---
+++ core/common/multithreading/thread_pool_test.cpp
@@ -0,0 +1,39 @@
+#include "thread_pool.h"
+#include "util/atomic.h"
+
+#include <folly/experimental/TestUtil.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <random>
+
+namespace sics::graph::core::common {
+
+// The fixture for testing class TheadPoolTest.
+class TheadPoolTest : public ::testing::Test {
+ protected:
+  TheadPoolTest() {
+    // Suppress death test warnings.
+    ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+  }
+};
+
+TEST_F(TheadPoolTest, CountShouldEqualsToK) {
+  auto parallelism = std::thread::hardware_concurrency();
+  auto thread_pool = ThreadPool(parallelism);
+  auto task_package = TaskPackage();
+
+  int k = random() % (1 << 5);
+  int count = 0;
+  for (int i = 0; i < k; i++) {
+    auto task = std::bind([i, &parallelism, &count]() {
+      sics::graph::core::util::atomic::WriteAdd(&count, 1);
+      return;
+    });
+    task_package.push_back(task);
+  }
+  thread_pool.SubmitSync(task_package);
+  EXPECT_EQ(k, count);
+}
+
+}  // namespace sics::graph::core::common
