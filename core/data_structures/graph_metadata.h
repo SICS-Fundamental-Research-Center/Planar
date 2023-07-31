26677ba7464de5c3ac71a6efe47bba4184db3fb9
liuyang <1844061236@buaa.edu.cn>
2023-07-31 14:25:21 +0800

    fix format: set and get functions; yaml template

--- core/data_structures/graph_metadata.h
+++ core/data_structures/graph_metadata.h
@@ -25,12 +25,12 @@ class GraphMetadata {
   // maybe used later
   GraphMetadata(const std::string& root_path);
 
-  void SetNumVertices(size_t num_vertices) { num_vertices_ = num_vertices; }
-  void SetNumEdges(size_t num_edges) { num_edges_ = num_edges; }
-  void SetNumSubgraphs(size_t num_subgraphs) { num_subgraphs_ = num_subgraphs; }
-  size_t GetNumVertices() const { return num_vertices_; }
-  size_t GetNumEdges() const { return num_edges_; }
-  size_t GetNumSubgraphs() const { return num_subgraphs_; }
+  void set_num_vertices(size_t num_vertices) { num_vertices_ = num_vertices; }
+  void set_num_edges(size_t num_edges) { num_edges_ = num_edges; }
+  void set_num_subgraphs(size_t num_subgraphs) { num_subgraphs_ = num_subgraphs; }
+  size_t get_num_vertices() const { return num_vertices_; }
+  size_t get_num_edges() const { return num_edges_; }
+  size_t get_num_subgraphs() const { return num_subgraphs_; }
 
   void AddSubgraphMetadata(SubgraphMetadata& subgraphMetadata) {
     subgraph_metadata_.emplace_back(subgraphMetadata);
@@ -60,5 +60,72 @@ class GraphMetadata {
 };
 
 }  // namespace sics::graph::core::data_structures
+
+// used for read meta.yaml when serialized(encode) and deserialize(decode)
+namespace YAML {
+
+// template is needed for this function
+template <>
+struct convert<sics::graph::core::data_structures::SubgraphMetadata> {
+  static Node encode(const sics::graph::core::data_structures::SubgraphMetadata&
+                         subgraph_metadata) {
+    Node node;
+    node["gid"] = subgraph_metadata.gid_;
+    node["num_vertices"] = subgraph_metadata.num_vertices_;
+    node["num_edges"] = subgraph_metadata.num_edges_;
+    node["size"] = subgraph_metadata.size_;
+    return node;
+  }
+  static bool decode(
+      const Node& node,
+      sics::graph::core::data_structures::SubgraphMetadata& subgraph_metadata) {
+    if (node.size() != 4) {
+      return false;
+    }
+    subgraph_metadata.gid_ =
+        node["gid"].as<sics::graph::core::common::GraphID>();
+    subgraph_metadata.num_vertices_ = node["num_vertices"].as<size_t>();
+    subgraph_metadata.num_edges_ = node["num_edges"].as<size_t>();
+    subgraph_metadata.size_ = node["size"].as<size_t>();
+    return true;
+  }
+};
+
+// template is needed for this function
+template<>
+struct convert<sics::graph::core::data_structures::GraphMetadata> {
+  static Node encode(
+      const sics::graph::core::data_structures::GraphMetadata& metadata) {
+    Node node;
+    node["num_vertices"] = metadata.get_num_vertices();
+    node["num_edges"] = metadata.get_num_edges();
+    node["num_subgraphs"] = metadata.get_num_subgraphs();
+    node["subgraphs"] =
+        std::vector<sics::graph::core::data_structures::SubgraphMetadata>();
+    for (int i = 0; i < metadata.get_num_subgraphs(); i++) {
+    }
+    return node;
+  }
+
+  static bool decode(
+      const Node& node,
+      sics::graph::core::data_structures::GraphMetadata& metadata) {
+    if (node.size() != 4) {
+      return false;
+    }
+    metadata.set_num_vertices(node["num_vertices"].as<size_t>());
+    metadata.set_num_edges(node["num_edges"].as<size_t>());
+    metadata.set_num_subgraphs(node["num_subgraphs"].as<size_t>());
+    auto subgraphMetadatas =
+        node["subgraphs"]
+            .as<std::vector<
+                sics::graph::core::data_structures::SubgraphMetadata>>();
+    for (int i = 0; i < metadata.get_num_subgraphs(); i++) {
+      metadata.AddSubgraphMetadata(subgraphMetadatas[i]);
+    }
+    return true;
+  }
+};
+}  // namespace YAML
 
 #endif  // GRAPH_SYSTEMS_GRAPH_METADATA_H
--- core/data_structures/graph_metadata_test.cpp
+++ core/data_structures/graph_metadata_test.cpp
@@ -17,12 +17,12 @@ TEST_F(GraphMetadataTest, NodeStructure) {
   YAML::Node metadata;
   metadata = YAML::LoadFile("../../testfile/meta.yaml");
   auto graph_metadata = metadata["GraphMetadata"].as<GraphMetadata>();
-  EXPECT_EQ(graph_metadata.GetNumVertices(), 56);
-  EXPECT_EQ(graph_metadata.GetNumEdges(), 100);
-  EXPECT_EQ(graph_metadata.GetNumSubgraphs(), 2);
+  EXPECT_EQ(graph_metadata.get_num_vertices(), 56);
+  EXPECT_EQ(graph_metadata.get_num_edges(), 100);
+  EXPECT_EQ(graph_metadata.get_num_subgraphs(), 2);
   std::vector<std::vector<int>> result_subgraph_metadata = {{28, 61, 100},
                                                             {28, 39, 101}};
-  for (int i = 0; i < graph_metadata.GetNumSubgraphs(); i++) {
+  for (int i = 0; i < graph_metadata.get_num_subgraphs(); i++) {
     auto subgraph_metadata = graph_metadata.GetSubgraphMetadata(i);
     EXPECT_EQ(subgraph_metadata.num_vertices_, result_subgraph_metadata[i][0]);
     EXPECT_EQ(subgraph_metadata.num_edges_, result_subgraph_metadata[i][1]);
