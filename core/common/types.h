4754a3a8456587554b37208f446b262491a11ba2
hsiaoko <zhuxk@buaa.edu.cn>
2023-08-01 04:01:30 +0000

    Formatted files.

--- core/common/types.h
+++ core/common/types.h
@@ -9,8 +9,6 @@ typedef uint32_t GraphID;  // uint32_t: 0 ~ 4,294,967,295
 typedef uint32_t VertexID;
 typedef uint32_t VertexLabel;
 
-#define ALIGNMENT(X) ((x >> 6) << 6) + 64
-
 }  // namespace sics::graph::core::common
 
 #endif  // CORE_COMMON_TYPES_H_
