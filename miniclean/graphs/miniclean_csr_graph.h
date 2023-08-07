a1677dda2e5c70f948b091a7d7a46dc234b2e5f7
bai-wenchao <wenchao.bai.chn@gmail.com>
2023-08-07 09:45:18 +0000

    created miniclean_csr_graph.h

---
+++ miniclean/graphs/miniclean_csr_graph.h
@@ -0,0 +1,13 @@
+#ifndef MINICLEAN_GRAPHS_MINICLEAN_CSR_GRAPH_H_
+#define MINICLEAN_GRAPHS_MINICLEAN_CSR_GRAPH_H_
+
+#include "core/data_structures/graph/immutable_csr_graph.h"
+
+namespace sics::graph::miniclean::graphs {
+
+class MinicleanCSRGraph : public sics::graph::core::data_structures::graph::ImmutableCSRGraph {
+    
+};
+}
+
+#endif  // MINICLEAN_GRAPHS_MINICLEAN_CSR_GRAPH_H_
