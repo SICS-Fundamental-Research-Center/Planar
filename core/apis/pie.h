c5d87107ea33dc762929f46f108c8c132c24731f
Shuhao Liu <liushuhaothu@gmail>
2023-08-08 20:17:49 +0800

    Added API base class PIE.

---
+++ core/apis/pie.h
@@ -0,0 +1,50 @@
+#ifndef GRAPH_SYSTEMS_PIE_H
+#define GRAPH_SYSTEMS_PIE_H
+
+#include <type_traits>
+
+#include "common/multithreading/task_runner.h"
+#include "data_structures/serializable.h"
+
+namespace sics::graph::core::apis {
+
+// PIE interfaces for graph applications.
+//
+// Each application should be defined by inheriting the PIE with a
+// concrete graph type, and implement the following methods:
+// - PEval: the initial evaluation phase.
+// - IncEval: the incremental evaluation phase.
+// - Assemble: the final assembly phase.
+template <typename GraphType>
+class PIE {
+  static_assert(
+      std::is_base_of<data_structures::Serializable, GraphType>::value,
+      "GraphType must be a subclass of Serializable");
+
+ public:
+  explicit PIE(common::TaskRunner* runner) : runner_(runner) {}
+
+  virtual ~PIE() = default;
+
+  // TODO: add UpdateStore as a parameter, so that PEval can save global
+  //  messages to it.
+  virtual void PEval(GraphType* graph) = 0;
+
+  // TODO: add a const UpdateStore as input for the last round, and a mutable
+  //  UpdateStore as output for the current round.
+  virtual void IncEval(GraphType* graph) = 0;
+
+  virtual void Assemble(GraphType* graph) = 0;
+
+ protected:
+  // TODO: implement this here.
+  virtual void VApply(GraphType* graph) = 0;
+  virtual void EApply(GraphType* graph) = 0;
+
+ protected:
+  common::TaskRunner* runner_;
+};
+
+}  // namespace sics::graph::core::apis
+
+#endif  // GRAPH_SYSTEMS_PIE_H
