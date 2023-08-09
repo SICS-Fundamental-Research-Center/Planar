fb1ab0aab46424ed8a6d110d1caba991e648e01b
hsiaoko <zhuxk@buaa.edu.cn>
2023-08-09 09:25:30 +0000

    Define MAX_VERTEX_ID

--- core/common/types.h
+++ core/common/types.h
@@ -9,7 +9,7 @@ typedef uint32_t GraphID;  // uint32_t: 0 ~ 4,294,967,295
 typedef uint32_t VertexID;
 typedef uint32_t VertexLabel;
 
-#define MAX_VERTEX_ID 0xFFFFFFFF
+#define MAX_VERTEX_ID std::numeric_limits<VertexID>::max()
 
 }  // namespace sics::graph::core::common
 
