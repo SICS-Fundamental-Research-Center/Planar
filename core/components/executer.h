a85f3e5cf2d4ec66f0238bd97a838cc92bba1380
liuyang <1844061236@buaa.edu.cn>
2023-08-10 18:10:11 +0800

    add: basic executer component

---
+++ core/components/executer.h
@@ -0,0 +1,54 @@
+#ifndef GRAPH_SYSTEMS_EXECUTER_H
+#define GRAPH_SYSTEMS_EXECUTER_H
+
+#include "components/component.h"
+#include "io/reader_writer.h"
+#include "scheduler/message_hub.h"
+#include "util/logging.h"
+#include <memory>
+#include <thread>
+#include <type_traits>
+
+namespace sics::graph::core::components {
+
+//template <typename GraphType>
+class Executer : public Component {
+ public:
+  Executer(scheduler::MessageHub* hub)
+      : execute_q_(hub->get_executor_queue()),
+        response_q_(hub->get_response_queue()) {}
+  ~Executer() final = default;
+
+  void Start() override {
+    thread_ = std::make_unique<std::thread>([this]() {
+      while (true) {
+        scheduler::ExecuteMessage message = execute_q_->PopOrWait();
+        if (message.terminated) {
+          LOG_INFO("*** Executer is signaled termination ***");
+          break;
+        }
+
+        LOGF_INFO("Executer starts executing subgraph {}", message.graph_id);
+        // TODO: execute api logic
+        LOGF_INFO("Executer completes executing subgraph {}", message.graph_id);
+        response_q_->Push(scheduler::Message(message));
+      }
+    });
+  }
+
+  void StopAndJoin() override {
+    scheduler::ExecuteMessage message;
+    message.terminated = true;
+    execute_q_->Push(message);
+    thread_->join();
+  }
+
+ private:
+  scheduler::ExecutorQueue* execute_q_;
+  scheduler::ResponseQueue* response_q_;
+  std::unique_ptr<std::thread> thread_;
+};
+
+}  // namespace sics::graph::core::components
+
+#endif  // GRAPH_SYSTEMS_EXECUTER_H
