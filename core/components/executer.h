e2fa6d7828839665e222713200e642c1a23cb96b
liuyang <1844061236@buaa.edu.cn>
2023-08-10 18:16:42 +0800

    fix: add executer pie template typename

--- core/components/executer.h
+++ core/components/executer.h
@@ -1,18 +1,24 @@
 #ifndef GRAPH_SYSTEMS_EXECUTER_H
 #define GRAPH_SYSTEMS_EXECUTER_H
 
-#include "components/component.h"
-#include "io/reader_writer.h"
-#include "scheduler/message_hub.h"
-#include "util/logging.h"
 #include <memory>
 #include <thread>
 #include <type_traits>
 
+#include "apis/pie.h"
+#include "components/component.h"
+#include "io/reader_writer.h"
+#include "scheduler/message_hub.h"
+#include "util/logging.h"
+
 namespace sics::graph::core::components {
 
-//template <typename GraphType>
+template <typename PIEApp>
 class Executer : public Component {
+ protected:
+  static_assert(std::is_base_of<apis::PIE, PIEApp>::value,
+                "PIEApp must be a subclass of PIEApp");
+
  public:
   Executer(scheduler::MessageHub* hub)
       : execute_q_(hub->get_executor_queue()),
@@ -30,6 +36,11 @@ class Executer : public Component {
 
         LOGF_INFO("Executer starts executing subgraph {}", message.graph_id);
         // TODO: execute api logic
+        if (message.execute_type == scheduler::ExecuteType::kPEval) {
+          app_->PEval();
+        } else if (message.execute_type == scheduler::ExecuteType::kIncEval) {
+          app_->IncEval();
+        }
         LOGF_INFO("Executer completes executing subgraph {}", message.graph_id);
         response_q_->Push(scheduler::Message(message));
       }
@@ -44,8 +55,11 @@ class Executer : public Component {
   }
 
  private:
+  PIEApp app_;
+
   scheduler::ExecutorQueue* execute_q_;
   scheduler::ResponseQueue* response_q_;
+
   std::unique_ptr<std::thread> thread_;
 };
 
