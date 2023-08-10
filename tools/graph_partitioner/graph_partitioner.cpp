7bd26b42ef4d3b19e7f0bc2bfdb81b61838b9895
hsiaoko <zhuxk@buaa.edu.cn>
2023-08-10 12:01:22 +0000

    Minor changes.

--- tools/graph_partitioner/graph_partitioner.cpp
+++ tools/graph_partitioner/graph_partitioner.cpp
@@ -412,7 +412,7 @@ int main(int argc, char** argv) {
       "list\n"
       "\t vertexcut: - Using vertex cut partitioner"
       "csr\n"
-      "\t hybridcut:   - Using hyrbid cut partitioner "
+      "\t hybridcut:   - Using hybrid cut partitioner "
       "csr\n");
 
   gflags::ParseCommandLineFlags(&argc, &argv, true);
