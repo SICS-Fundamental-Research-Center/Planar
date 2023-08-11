e95a4859100ac309fd5be636e4d80d20ec10cef5
liuyang <1844061236@buaa.edu.cn>
2023-08-11 17:08:14 +0800

    add: message_stores directory and base class of MessageStore

---
+++ core/message_stores/message_store_base.h
@@ -0,0 +1,14 @@
+#ifndef GRAPH_SYSTEMS_MESSAGE_STORE_BASE_H
+#define GRAPH_SYSTEMS_MESSAGE_STORE_BASE_H
+
+namespace sics::graph::core::message_stores {
+
+class MessageStoreBase {
+ public:
+  virtual void Clear() = 0;
+
+};
+
+}  // namespace sics::graph::core::message_stores
+
+#endif  // GRAPH_SYSTEMS_MESSAGE_STORE_BASE_H
