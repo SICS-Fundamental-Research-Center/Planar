6f014c9641a765dc2375cf34dd36b847f8dd39a6
hsiaoko <zhuxk@buaa.edu.cn>
2023-08-16 08:05:03 +0000

    Revised ImmutableCSRGraph class.

--- core/data_structures/graph/immutable_csr_graph.h
+++ core/data_structures/graph/immutable_csr_graph.h
@@ -1,3 +1,4 @@
+
 #ifndef CORE_DATA_STRUCTURES_GRAPH_IMMUTABLE_CSR_GRAPH_H_
 #define CORE_DATA_STRUCTURES_GRAPH_IMMUTABLE_CSR_GRAPH_H_
 
@@ -22,11 +23,29 @@ struct ImmutableCSRVertex {
   using SubgraphMetadata = sics::graph::core::data_structures::SubgraphMetadata;
 
  public:
+  void ShowVertex() {
+    std::cout << "  ===vid: " << vid << ", indegree: " << indegree
+              << ", outdegree: " << outdegree << "===" << std::endl;
+    if (indegree != 0) {
+      std::cout << "    Incoming edges: ";
+      for (VertexID i = 0; i < indegree; i++)
+        std::cout << incoming_edges[i] << ",";
+      std::cout << std::endl << std::endl;
+    }
+    if (outdegree != 0) {
+      std::cout << "    Outgoing edges: ";
+      for (VertexID i = 0; i < outdegree; i++)
+        std::cout << outgoing_edges[i] << ",";
+      std::cout << std::endl << std::endl;
+    }
+    std::cout << "****************************************" << std::endl;
+  }
+
   VertexID vid;
-  VertexID indegree;
-  VertexID outdegree;
-  VertexID* in_edges;
-  VertexID* out_edges;
+  VertexID indegree = 0;
+  VertexID outdegree = 0;
+  VertexID* incoming_edges;
+  VertexID* outgoing_edges;
 };
 
 class ImmutableCSRGraph : public Serializable {
@@ -35,12 +54,10 @@ class ImmutableCSRGraph : public Serializable {
   using VertexID = sics::graph::core::common::VertexID;
 
  public:
-  ImmutableCSRGraph(const GraphID gid) : Serializable(), gid_(gid) {}
-  ImmutableCSRGraph(const GraphID gid, ImmutableCSRGraphConfig&& csr_config)
-      : Serializable(), gid_(gid), csr_config_(std::move(csr_config)) {}
-
-  explicit ImmutableCSRGraph(SubgraphMetadata& metadata)
+  explicit ImmutableCSRGraph(SubgraphMetadata metadata)
       : Serializable(), metadata_(metadata) {}
+
+  ImmutableCSRGraph(GraphID gid) : Serializable(), gid_(gid) {}
 
   std::unique_ptr<Serialized> Serialize(
       const common::TaskRunner& runner) override;
@@ -48,38 +65,104 @@ class ImmutableCSRGraph : public Serializable {
   void Deserialize(const common::TaskRunner& runner,
                    std::unique_ptr<Serialized>&& serialized) override;
 
-  void set_gid(GraphID gid) { gid_ = gid; }
-  void set_num_vertices(VertexID val) { num_vertices_ = val; }
-  void set_num_incoming_edges(VertexID val) { num_incoming_edges_ = val; }
-  void set_num_outgoing_edges(VertexID val) { num_outgoing_edges_ = val; }
-  void set_max_vid(VertexID val) { max_vid_ = val; }
-  void set_min_vid(VertexID val) { min_vid_ = val; }
-
-  GraphID get_gid() const { return gid_; }
-  VertexID get_num_vertices() const { return num_vertices_; }
-  VertexID get_num_incoming_edges() const { return num_incoming_edges_; }
-  VertexID get_num_outgoing_edges() const { return num_outgoing_edges_; }
-  VertexID get_max_vid() const { return max_vid_; }
-  VertexID get_min_vid() const { return min_vid_; }
-
-  void SetGlobalID(VertexID* globalid_by_index) {
-    globalid_by_index_ = globalid_by_index;
+  void ShowGraph(VertexID display_num = 0) {
+    LOG_INFO("### GID: ", gid_, ",  num_vertices: ", num_vertices_,
+             ", num_incoming_edges: ", num_incoming_edges_,
+             ", num_outgoing_edges: ", num_outgoing_edges_, " ###");
+    for (VertexID i = 0; i < display_num; i++) {
+      if (i >= num_vertices_) break;
+      auto u = GetVertexByLocalID(i);
+      u.ShowVertex();
+    }
   }
-  void SetInDegree(VertexID* indegree) { indegree_ = indegree; }
-  void SetOutDegree(VertexID* outdegree) { outdegree_ = outdegree; }
-  void SetInOffset(VertexID* in_offset) { in_offset_ = in_offset; }
-  void SetOutOffset(VertexID* out_offset) { out_offset_ = out_offset; }
-  void SetInEdges(VertexID* in_edges) { in_edges_ = in_edges; }
-  void SetOutEdges(VertexID* out_edges) { out_edges_ = out_edges; }
 
-  uint8_t* GetGraphBuffer() const { return buf_graph_; }
-  VertexID* GetGlobalIDBuffer() const { return globalid_by_index_; }
-  VertexID* GetInEdges() const { return in_edges_; }
-  VertexID* GetOutEdges() const { return out_edges_; }
-  VertexID* GetInDegree() const { return indegree_; }
-  VertexID* GetOutDegree() const { return outdegree_; }
-  VertexID* GetInOffset() const { return in_offset_; }
-  VertexID* GetOutOffset() const { return out_offset_; }
+  inline void set_gid(GraphID gid) { gid_ = gid; }
+  inline void set_num_vertices(VertexID val) { num_vertices_ = val; }
+  inline void set_num_incoming_edges(VertexID val) {
+    num_incoming_edges_ = val;
+  }
+  inline void set_num_outgoing_edges(VertexID val) {
+    num_outgoing_edges_ = val;
+  }
+  inline void set_max_vid(const VertexID val) { max_vid_ = val; }
+  inline void set_min_vid(const VertexID val) { min_vid_ = val; }
+
+  inline GraphID get_gid() const { return gid_; }
+  inline VertexID get_num_vertices() const { return num_vertices_; }
+  inline VertexID get_num_incoming_edges() const { return num_incoming_edges_; }
+  inline VertexID get_num_outgoing_edges() const { return num_outgoing_edges_; }
+  inline VertexID get_max_vid() const { return max_vid_; }
+  inline VertexID get_min_vid() const { return min_vid_; }
+
+  void SetGlobalIDBuffer(VertexID* buffer) {
+    globalid_by_localid_base_pointer_ = buffer;
+  }
+  void SetInDegreeBuffer(VertexID* buffer) { indegree_base_pointer_ = buffer; }
+  void SetOutDegreeBuffer(VertexID* buffer) {
+    outdegree_base_pointer_ = buffer;
+  }
+  void SetInOffsetBuffer(VertexID* buffer) { in_offset_base_pointer_ = buffer; }
+  void SetOutOffsetBuffer(VertexID* buffer) {
+    out_offset_base_pointer_ = buffer;
+  }
+  void SetIncomingEdgesBuffer(VertexID* buffer) {
+    incoming_edges_base_pointer_ = buffer;
+  }
+  void SetOutgoingEdgesBuffer(VertexID* buffer) {
+    outgoing_edges_base_pointer_ = buffer;
+  }
+
+  uint8_t* GetGraphBuffer() { return buf_graph_base_pointer_; }
+  VertexID* GetGloablIDBasePointer() {
+    return globalid_by_localid_base_pointer_;
+  }
+  VertexID* GetInDegreeBasePointer() { return indegree_base_pointer_; }
+  VertexID* GetOutDegreeBasePointer() { return outdegree_base_pointer_; }
+  VertexID* GetInOffsetBasePointer() { return in_offset_base_pointer_; }
+  VertexID* GetOutOffsetBasePointer() { return out_offset_base_pointer_; }
+  VertexID* GetIncomingEdgesBasePointer() {
+    return incoming_edges_base_pointer_;
+  }
+  VertexID* GetOutgoingEdgesBasePointer() {
+    return outgoing_edges_base_pointer_;
+  }
+
+  VertexID GetGlobalIDByLocalID(VertexID i) const {
+    return globalid_by_localid_base_pointer_[i];
+  }
+  inline VertexID GetInOffsetByLocalID(VertexID i) const {
+    return in_offset_base_pointer_[i];
+  }
+  inline VertexID GetOutOffsetByLocalID(VertexID i) const {
+    return out_offset_base_pointer_[i];
+  }
+  inline VertexID* GetIncomingEdgesByLocalID(VertexID i) {
+    return incoming_edges_base_pointer_ + i;
+  }
+  inline VertexID* GetOutgoingEdgesByLocalID(VertexID i) {
+    return outgoing_edges_base_pointer_ + i;
+  }
+  inline VertexID GetInDegreeByLocalID(VertexID i) const {
+    return indegree_base_pointer_[i];
+  }
+  inline VertexID GetOutDegreeByLocalID(VertexID i) const {
+    return outdegree_base_pointer_[i];
+  }
+
+  inline ImmutableCSRVertex GetVertexByLocalID(VertexID i) const {
+    ImmutableCSRVertex v;
+    v.vid = GetGlobalIDByLocalID(i);
+    if (num_incoming_edges_ > 0) {
+      v.indegree = GetInDegreeByLocalID(i);
+      v.incoming_edges = incoming_edges_base_pointer_ + GetInOffsetByLocalID(i);
+    }
+    if (num_outgoing_edges_ > 0) {
+      v.outdegree = GetOutDegreeByLocalID(i);
+      v.outgoing_edges =
+          outgoing_edges_base_pointer_ + GetOutOffsetByLocalID(i);
+    }
+    return v;
+  }
 
  private:
   void ParseSubgraphCSR(const std::list<OwnedBuffer>& buffer_list);
@@ -94,21 +177,18 @@ class ImmutableCSRGraph : public Serializable {
   VertexID max_vid_ = 0;
   VertexID min_vid_ = 0;
 
-  uint8_t* buf_graph_;
-
-  // config. attributes to build the CSR.
-  ImmutableCSRGraphConfig csr_config_;
+  // config. Metadata to build the CSR.
   SubgraphMetadata metadata_;
 
   // serialized data in CSR format.
-  VertexID* globalid_by_index_;
-  VertexID* in_edges_;
-  VertexID* out_edges_;
-  VertexID* indegree_;
-  VertexID* outdegree_;
-  VertexID* in_offset_;
-  VertexID* out_offset_;
-  VertexID* localid_by_globalid_;
+  uint8_t* buf_graph_base_pointer_;
+  VertexID* globalid_by_localid_base_pointer_;
+  VertexID* incoming_edges_base_pointer_;
+  VertexID* outgoing_edges_base_pointer_;
+  VertexID* indegree_base_pointer_;
+  VertexID* outdegree_base_pointer_;
+  VertexID* in_offset_base_pointer_;
+  VertexID* out_offset_base_pointer_;
 };
 
 }  // namespace sics::graph::core::data_structures::graph
