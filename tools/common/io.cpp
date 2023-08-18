906b1fa15df200be0925c0a2d8db31cf221f4f2e
Xiaoke.Zhu <xiaoke.zhu@outlook.com>
2023-08-18 09:30:54 +0800

    Update tools/common/io.cpp
    Co-authored-by: Shuhao Liu <liushuhaothu@gmail.com>

--- tools/common/io.cpp
+++ tools/common/io.cpp
@@ -151,7 +151,8 @@ bool IOConverter::WriteSubgraph(
       case kUnconstrained:
         out_data_file.write((char*)buffer_in_edges,
                             sizeof(VertexID) * count_in_edges);
-        out_data_file.write((char*)buffer_out_edges,
+        out_data_file.write((char*) buffer_out_edges,
+
                             sizeof(VertexID) * count_out_edges);
         break;
       case kUndefinedStrategy:
