12a73c3436947d957cdbcd0459644e03be7d6921
liuyang <1844061236@buaa.edu.cn>
2023-08-28 17:15:01 +0800

    add logic

--- core/apps/wcc_app.cpp
+++ core/apps/wcc_app.cpp
@@ -24,4 +24,68 @@ void WCCApp::IncEval() {
 
 void WCCApp::Assemble() { graph_->set_status("Assemble"); }
 
+void WCCApp::init(VertexID id) { graph_->Write(id, WccVertexData(id)); }
+
+void WCCApp::graft(VertexID src_id, VertexID dst_id) {
+  VertexID src_parent_id = graph_->Read(src_id)->p;
+  VertexID dst_parent_id = graph_->Read(dst_id)->p;
+  if (src_parent_id != dst_parent_id) {
+    if (src_parent_id < dst_parent_id) {
+      graph_->Write(dst_parent_id,
+                    WccVertexData(graph_->Read(src_parent_id)->p));
+    } else {
+      graph_->Write(src_parent_id,
+                    WccVertexData(graph_->Read(dst_parent_id)->p));
+    }
+  }
+  // TODO: add UpdateStore info logic
+}
+
+void WCCApp::point_jump(sics::graph::core::apps::WCCApp::VertexID src_id) {
+  VertexData parent;
+  parent.p = graph_->Read(src_id)->p;
+  if (parent.p != graph_->Read(parent.p)->p) {
+    while (parent.p != graph_->Read(parent.p)->p) {
+      parent.p = graph_->Read(parent.p)->p;
+    }
+    graph_->Write(src_id, parent);
+  }
+  // TODO: update vertex global data in update_store
+}
+
+void WCCApp::contract(VertexID src_id, VertexID dst_id, EdgeIndex idx) {
+  if (graph_->Read(src_id)->p == graph_->Read(dst_id)->p) {
+    graph_->DeleteEdge(idx);
+  }
+}
+
+void WCCApp::message_passing(sics::graph::core::apps::WCCApp::VertexID id) {
+  if (update_store_->Read(id)->p < graph_->Read(id)->p) {
+    if (!graph_->IsInGraph(graph_->Read(id)->p)) {
+      mtx.lock();
+      id_to_p[graph_->Read(id)->p] = update_store_->Read(id)->p;
+      mtx.unlock();
+    } else {
+      // TODO: active vertex update global info
+      graph_->Write(graph_->Read(id)->p,
+                    WccVertexData(update_store_->Read(id)->p));
+    }
+  }
+}
+
+void WCCApp::point_jump_inc(VertexID id) {
+  // is not in graph
+  if (!graph_->IsInGraph(graph_->Read(id)->p)) {
+    mtx.lock();
+    id_to_p[graph_->Read(id)->p] = graph_->Read(id)->p;
+    mtx.unlock();
+  } else {
+    bool flag =
+        graph_->Write(id, WccVertexData(graph_->Read(graph_->Read(id)->p)->p));
+    if (flag) {
+      // TODO: active vertex update global info
+    }
+  }
+}
+
 }  // namespace sics::graph::core::apps
--- core/apps/wcc_app.h
+++ core/apps/wcc_app.h
@@ -35,56 +35,18 @@ class WCCApp : public apis::PlanarAppBase<CSRGraph> {
   void Assemble() final;
 
  private:
-  void init(VertexID id) { graph_->Write(id, WccVertexData(id)); }
+  void init(VertexID id);
 
-  void graft(VertexID src_id, VertexID dst_id) {
-    VertexID src_parent_id = graph_->Read(src_id)->p;
-    VertexID dst_parent_id = graph_->Read(dst_id)->p;
-    if (src_parent_id != dst_parent_id) {
-      if (src_parent_id < dst_parent_id) {
-        graph_->Write(dst_parent_id,
-                      WccVertexData(graph_->Read(src_parent_id)->p));
-      } else {
-        graph_->Write(src_parent_id,
-                      WccVertexData(graph_->Read(dst_parent_id)->p));
-      }
-    }
-    // TODO: add UpdateStore info logic
-  }
+  void graft(VertexID src_id, VertexID dst_id);
 
-  void point_jump(VertexID src_id) {
-    VertexData parent;
-    parent.p = graph_->Read(src_id)->p;
-    if (parent.p != graph_->Read(parent.p)->p) {
-      while (parent.p != graph_->Read(parent.p)->p) {
-        parent.p = graph_->Read(parent.p)->p;
-      }
-      graph_->Write(src_id, parent);
-    }
-    // TODO: update vertex global data in update_store
-  }
+  void point_jump(VertexID src_id);
 
-  void contract(VertexID src_id, VertexID dst_id, EdgeIndex idx) {
-    if (graph_->Read(src_id)->p == graph_->Read(dst_id)->p) {
-      graph_->DeleteEdge(idx);
-    }
-  }
+  void contract(VertexID src_id, VertexID dst_id, EdgeIndex idx);
 
   // TODO: judge if the vertex is active, this can be done in parallelDo
-  void message_passing(VertexID id) {
-    if (update_store_->Read(id)->p < graph_->Read(id)->p) {
-      if (!graph_->IsInGraph(graph_->Read(id)->p)) {
-        mtx.lock();
-        id_to_p[graph_->Read(id)->p] = update_store_->Read(id)->p;
-        mtx.unlock();
-      } else {
+  void message_passing(VertexID id);
 
-        // TODO: active vertex update global info
-      }
-    }
-  }
-
-  void point_jump_inc(VertexID id) {}
+  void point_jump_inc(VertexID id);
 
  private:
   std::unordered_map<VertexID, VertexID> id_to_p;
