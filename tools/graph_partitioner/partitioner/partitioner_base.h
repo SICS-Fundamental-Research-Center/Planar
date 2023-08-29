2febf3d389dd0a88f61fdc44cba1296ee9a9b09e
hsiaoko <zhuxk@buaa.edu.cn>
2023-08-29 13:26:26 +0000

    Added PartitionerBase class.

---
+++ tools/graph_partitioner/partitioner/partitioner_base.h
@@ -0,0 +1,38 @@
+#ifndef SICS_GRAPH_SYSTEMS_TOOLS_PARTITIONER_H_
+#define SICS_GRAPH_SYSTEMS_TOOLS_PARTITIONER_H_
+
+#include "tools/common/types.h"
+
+namespace sics::graph::tools::partitioner {
+
+// This class defines the Partitioner interface, which is used to partition a
+// graph into several subgraphs. serialize and.
+class PartitionerBase {
+ private:
+  using StoreStrategy = sics::graph::tools::common::StoreStrategy;
+
+ public:
+  PartitionerBase(const std::string& input_path,
+                  const std::string& output_path,
+                  StoreStrategy store_strategy)
+      : input_path_(input_path),
+        output_path_(output_path),
+        store_strategy_(store_strategy) {}
+
+  // This function will submit the partitioning task.
+  virtual void RunPartitioner() = 0;
+
+
+ std::string GetInputPath() const { return input_path_; }
+ std::string GetOutputPath() const { return output_path_; }
+ StoreStrategy GetStoreStrategy() const { return store_strategy_; }
+
+ private:
+  const std::string input_path_;
+  const std::string output_path_;
+  const StoreStrategy store_strategy_;
+};
+
+}  // namespace sics::graph::tools::partitioner
+
+#endif  // SICS_GRAPH_SYSTEMS_TOOLS_PARTITIONER_H_
