c41570906c5e142c46d937bc78e9b9b30345bfbe
hsiaoko <zhuxk@buaa.edu.cn>
2023-09-03 04:08:31 +0000

    Added edge structure that be used in Planar partitioner.

--- tools/common/data_structures.h
+++ tools/common/data_structures.h
@@ -3,10 +3,37 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <vector>
 
 #include "core/common/types.h"
 
 namespace sics::graph::tools::common {
+
+class Edge {
+ private:
+  using VertexID = sics::graph::core::common::VertexID;
+
+ public:
+  Edge(VertexID src, VertexID dst) : src_(src), dst_(dst) {}
+
+  static void SortBySrc(std::vector<Edge>& edges) {
+    sort(edges.begin(), edges.end(),
+         [](const Edge& l, const Edge& r) { return l < r; });
+  }
+
+  static void SortByDst(std::vector<Edge>& edges) {
+    sort(edges.begin(), edges.end(),
+         [](const Edge& l, const Edge& r) { return l.dst_ < r.dst_; });
+  }
+
+  bool operator<(const Edge& e) const { return src_ < e.src_; }
+  bool operator>(const Edge& e) const { return src_ > e.src_; }
+  bool operator-(const Edge& e) const { return src_ - e.src_; }
+
+ public:
+  VertexID src_;
+  VertexID dst_;
+};
 
 struct EdgelistMetadata {
  private:
