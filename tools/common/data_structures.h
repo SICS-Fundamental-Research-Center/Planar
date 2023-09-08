027fb2f327b3c6e86bbc296532eb5fee7a87408d
hsiaoko <zhuxk@buaa.edu.cn>
2023-09-08 07:30:43 +0000

    Added binary search in Edges.

--- tools/common/data_structures.h
+++ tools/common/data_structures.h
@@ -3,12 +3,28 @@
 
 #include <cstddef>  // For std::ptrdiff_t
 #include <cstdint>
+#include <iostream>
 #include <iterator>  // For std::forward_iterator_tag
 #include <vector>
 
+#include "core/common/bitmap.h"
+#include "core/common/multithreading/thread_pool.h"
 #include "core/common/types.h"
+#include "core/util/atomic.h"
+#include "core/util/logging.h"
+#include "tools/common/data_structures.h"
 
 namespace sics::graph::tools::common {
+
+struct EdgelistMetadata {
+ private:
+  using VertexID = sics::graph::core::common::VertexID;
+
+ public:
+  size_t num_vertices;
+  size_t num_edges;
+  VertexID max_vid;
+};
 
 struct Edge {
  private:
@@ -16,6 +32,7 @@ struct Edge {
 
  public:
   Edge(VertexID src, VertexID dst) : src(src), dst(dst) {}
+  Edge() = default;
 
   bool operator<(const Edge& e) const {
     if (src != e.src) {
@@ -25,12 +42,23 @@ struct Edge {
     }
   }
 
+  bool operator>(VertexID n) const { return src > n; }
+  bool operator<(VertexID n) const { return src < n; }
+
+  bool operator()(VertexID n, const Edge& e) const { return n > e.src; }
+
  public:
   VertexID src;
   VertexID dst;
 };
 
 class Edges {
+ private:
+  using VertexID = sics::graph::core::common::VertexID;
+  using Bitmap = sics::graph::core::common::Bitmap;
+  using TaskPackage = sics::graph::core::common::TaskPackage;
+  using EdgelistMetadata = sics::graph::tools::common::EdgelistMetadata;
+
  public:
   struct Iterator {
     // Iterator tags
@@ -89,6 +117,7 @@ class Edges {
     bool operator>=(const Iterator& b) const {
       return (*base_ptr_).src >= (*b).src;
     }
+
     friend bool operator==(const Iterator& a, const Iterator& b) {
       return a.base_ptr_ == b.base_ptr_;
     };
@@ -96,46 +125,107 @@ class Edges {
       return a.base_ptr_ != b.base_ptr_;
     };
 
+    pointer get_base_ptr() const { return base_ptr_; };
+
    private:
     pointer base_ptr_;
   };
 
-  Edges(size_t n_edges, Edge* edges_ptr)
-      : n_edges_(n_edges), edges_ptr_(edges_ptr) {}
+  Edges(const EdgelistMetadata& edgelist_metadata, Edge* edges_ptr)
+      : edgelist_metadata_(edgelist_metadata), edges_ptr_(edges_ptr) {}
+
+  Edges(const EdgelistMetadata& edgelist_metadata)
+      : edgelist_metadata_(edgelist_metadata) {
+    edges_ptr_ = new Edge[edgelist_metadata.num_edges]();
+  }
 
   ~Edges() { delete edges_ptr_; }
 
   Iterator begin() { return Iterator(&edges_ptr_[0]); }
-  Iterator end() { return Iterator(&edges_ptr_[n_edges_ - 1]); }
+  Iterator end() {
+    return Iterator(&edges_ptr_[edgelist_metadata_.num_edges - 1]);
+  }
 
-  static void SortBySrc(Edges edges) {
-    std::sort(std::begin(edges), std::end(edges),
+  void SortBySrc() {
+    std::sort(std::begin(*this), std::end(*this),
               [](const Edge& l, const Edge& r) {
                 if (l.src != r.src) {
                   return l.src < r.src;
-                } else {
+                }
+                if (l.dst != r.dst) {
                   return l.dst < r.dst;
                 }
               });
   }
 
-  static void SortByDst(Edges edges) {
-    // TODO (hsiaoko):
+  void SortByDst() {
+    std::sort(std::begin(*this), std::end(*this),
+              [](const Edge& l, const Edge& r) {
+                if (l.dst != r.dst) {
+                  return l.dst < r.dst;
+                }
+                if (l.src != r.src) {
+                  return l.src < r.src;
+                }
+              });
   }
 
+  // count the number of vertices and find the vertex with maximum
+  // outdegree.
+  VertexID GetVertexWithMaximumDegree() {
+    auto parallelism = std::thread::hardware_concurrency();
+    auto thread_pool = sics::graph::core::common::ThreadPool(parallelism);
+    auto task_package = TaskPackage();
+    std::mutex mtx;
+
+    auto aligned_max_vid = ((edgelist_metadata_.max_vid >> 6) << 6) + 64;
+    VertexID outdegree_by_vid[aligned_max_vid] = {0};
+    VertexID max_outdegree = 0, vid_with_maximum_degree = 0;
+
+    for (unsigned int i = 0; i < parallelism; i++) {
+      auto task = std::bind([&, i]() {
+        for (VertexID j = i; j < edgelist_metadata_.num_edges;
+             j += parallelism) {
+          auto src = get_src_by_index(j);
+          sics::graph::core::util::atomic::WriteAdd(outdegree_by_vid + src,
+                                                    (VertexID)1);
+          std::lock_guard<std::mutex> lck(mtx);
+          if (sics::graph::core::util::atomic::WriteMax(
+                  &max_outdegree, outdegree_by_vid[src])) {
+            vid_with_maximum_degree = src;
+          }
+        }
+      });
+      task_package.push_back(task);
+    }
+    thread_pool.SubmitSync(task_package);
+    task_package.clear();
+
+    return vid_with_maximum_degree;
+  }
+
+  Iterator SearchVertex(VertexID vid) {
+    auto iter = std::lower_bound(this->begin(), this->end(), vid);
+    return iter;
+  }
+
+  void PushBack(const Edge& e) {}
+
+  size_t GetIndexByIter(const Iterator& iter) {
+    return (iter.get_base_ptr() - begin().get_base_ptr());
+  };
+
+  void AssignEdge(size_t pos, const Edge& e) { edges_ptr_[pos] = e; }
+
+  Edge* get_base_ptr() { return edges_ptr_; }
+  VertexID get_src_by_index(size_t i) const { return edges_ptr_[i].src; }
+  VertexID get_dst_by_index(size_t i) const { return edges_ptr_[i].dst; }
+  Edge get_edge_by_index(size_t i) const { return edges_ptr_[i]; }
+  EdgelistMetadata get_metadata() const { return edgelist_metadata_; }
+
  private:
-  size_t n_edges_ = 0;
   Edge* edges_ptr_;
-};
-
-struct EdgelistMetadata {
- private:
-  using VertexID = sics::graph::core::common::VertexID;
-
- public:
-  size_t num_vertices;
-  size_t num_edges;
-  VertexID max_vid;
+  EdgelistMetadata edgelist_metadata_;
 };
 
 }  // namespace sics::graph::tools::common
