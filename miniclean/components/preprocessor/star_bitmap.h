cb1d9f950b03f31fea32020771c3dd217981a203
BaiWenchao <wenchao.bai@qq.com>
2023-09-13 17:16:39 +0800

    implemented a simple bitmap for star pattern.

---
+++ miniclean/components/preprocessor/star_bitmap.h
@@ -0,0 +1,44 @@
+#ifndef MINICLEAN_COMPONENTS_PREPROCESSOR_INDEX_METADATA_H_
+#define MINICLEAN_COMPONENTS_PREPROCESSOR_INDEX_METADATA_H_
+
+#include <bitset>
+#include <vector>
+
+namespace sics::graph::miniclean::components::preprocessor {
+
+#define WORD_OFFSET(i) (i >> 6)
+#define BIT_OFFSET(i) (i & 0x3f)
+
+class StarBitmap {
+ public:
+  StarBitmap(size_t map_size) : map_size_(map_size) {
+    size_t bitmap_size = WORD_OFFSET(map_size) + 1;
+    star_bitmap_.resize(bitmap_size);
+  }
+
+  void SetBit(size_t index) {
+    star_bitmap_[WORD_OFFSET(index)].set(BIT_OFFSET(index));
+  }
+
+  void MergeWith(StarBitmap& other) {
+    for (size_t i = 0; i < star_bitmap_.size(); ++i) {
+      star_bitmap_[i] &= other.star_bitmap_[i];
+    }
+  }
+
+  size_t CountOneBits() {
+    size_t num_one = 0;
+    for (size_t i = 0; i < star_bitmap_.size(); ++i) {
+      num_one += star_bitmap_[i].count();
+    }
+    return num_one;
+  }
+
+ private:
+  std::vector<std::bitset<64>> star_bitmap_;
+  size_t map_size_ = 0;
+};
+
+}  // namespace sics::graph::miniclean::components::preprocessor
+
+#endif  // MINICLEAN_COMPONENTS_PREPROCESSOR_INDEX_METADATA_H_
