dd4a2bb81804fe768448e3c02a1a69c132ba0230
liuyang <1844061236@buaa.edu.cn>
2023-09-19 17:41:05 +0800

    fix tools build error of clang

--- tools/graph_partitioner/partitioner/hash_based_vertexcut.cpp
+++ tools/graph_partitioner/partitioner/hash_based_vertexcut.cpp
@@ -117,8 +117,9 @@ void HashBasedVertexCutPartitioner::RunPartitioner() {
   std::vector<Edges> edge_buckets;
 
   for (size_t i = 0; i < n_partitions_; i++) {
-    EdgelistMetadata edgelist_metadata = {
-        bitmap_vec.at(i).Count(), size_per_bucket[i], max_vid_per_bucket[i]};
+    EdgelistMetadata edgelist_metadata = {(VertexID)(bitmap_vec.at(i).Count()),
+                                          size_per_bucket[i],
+                                          max_vid_per_bucket[i]};
     edgelist_metadata_vec.push_back(edgelist_metadata);
     edge_buckets.emplace_back(Edges(edgelist_metadata));
   }
