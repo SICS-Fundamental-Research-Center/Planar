ac5316b4f382c55bb416a6b51cd449552191e13a
hsiaoko <xiaoke.zhu@outlook.com>
2023-09-20 16:07:51 +0900

    fixed an error.

--- tools/common/io.cpp
+++ tools/common/io.cpp
@@ -59,8 +59,8 @@ void GraphFormatConverter::WriteSubgraph(
           buffer_globalid2index[bucket.at(j).vid] = j;
           buffer_indegree[j] = bucket.at(j).indegree;
           buffer_outdegree[j] = bucket.at(j).outdegree;
-          WriteAdd(&count_out_edges, (EdgeIndex) bucket.at(j).outdegree);
-          WriteAdd(&count_in_edges, (EdgeIndex) bucket.at(j).indegree);
+          WriteAdd(&count_out_edges, (EdgeIndex)bucket.at(j).outdegree);
+          WriteAdd(&count_in_edges, (EdgeIndex)bucket.at(j).indegree);
           WriteMin(&min_vid, buffer_globalid[j]);
           WriteMax(&max_vid, buffer_globalid[j]);
         }
@@ -143,6 +143,9 @@ void GraphFormatConverter::WriteSubgraph(
     thread_pool.SubmitSync(task_package);
     task_package.clear();
 
+    for (size_t i = 0; i < num_vertices; i++) {
+      LOG_INFO(buffer_out_offset[i]);
+    }
     delete[] buffer_in_offset;
     delete[] buffer_out_offset;
 
@@ -358,7 +361,7 @@ void GraphFormatConverter::WriteSubgraph(const std::vector<Edges>& edge_buckets,
         reinterpret_cast<char*>(csr_graph.GetGloablIDBasePointer()),
         sizeof(VertexID) * csr_graph.get_num_vertices());
 
-    // Write subgraph metadata.
+    //  Write subgraph metadata.
     switch (store_strategy) {
       case kOutgoingOnly:
         subgraph_metadata_vec.push_back(
