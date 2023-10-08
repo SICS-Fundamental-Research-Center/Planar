0e124ccedefd1a0ad6ec1243fd3339393437492a
bai-wenchao <wenchao.bai.chn@gmail.com>
2023-10-08 11:44:21 +0000

    removed `LoadPath`.

--- miniclean/components/preprocessor/index_collection.h
+++ miniclean/components/preprocessor/index_collection.h
@@ -95,12 +95,9 @@ class IndexCollection {
 
  public:
   IndexCollection() = default;
-  void LoadIndexCollection(const std::string& path_pattern_file,
-                           const std::string& attribute_config_file);
+  void LoadIndexCollection(const std::string& attribute_config_file);
 
  private:
-  void LoadPathPatterns(const std::string& path_pattern_file);
-  void LoadAttributeConfig(const std::string& attribute_config_file);
   void LoadVertexAttributeSegment(const std::string& vertex_attribute_file);
 
   std::vector<PathPattern> path_patterns_;
@@ -111,45 +108,10 @@ class IndexCollection {
 
 namespace YAML {
 
-using EdgePattern = sics::graph::miniclean::common::EdgePattern;
-using PathPattern = sics::graph::miniclean::common::PathPattern;
-using EdgeLabel = sics::graph::miniclean::common::EdgeLabel;
 using VertexLabel = sics::graph::miniclean::common::VertexLabel;
 using VertexAttributeSegment =
     sics::graph::miniclean::components::preprocessor::VertexAttributeSegment;
 using VertexAttributeID = sics::graph::miniclean::common::VertexAttributeID;
-using VertexID = sics::graph::miniclean::common::VertexID;
-
-template <>
-struct convert<PathPattern> {
-  static Node encode(const PathPattern& path_pattern) {
-    std::vector<uint8_t> path_pattern_node;
-    path_pattern_node.reserve(path_pattern.size() * 2 - 1);
-    for (size_t i = 0; i < path_pattern.size() - 1; ++i) {
-      path_pattern_node.push_back(std::get<0>(path_pattern[i]));
-      path_pattern_node.push_back(std::get<1>(path_pattern[i]));
-    }
-    path_pattern_node.push_back(
-        std::get<0>(path_pattern[path_pattern.size() - 1]));
-    Node node(path_pattern_node);
-    return node;
-  }
-
-  static bool decode(const Node& node, PathPattern& path_pattern) {
-    if (!node.IsSequence()) {
-      return false;
-    }
-    path_pattern.reserve(node.size() / 2 + 1);
-    for (size_t i = 0; i < node.size() - 1; i += 2) {
-      path_pattern.emplace_back(node[i].as<VertexLabel>(),
-                                node[i + 1].as<EdgeLabel>(),
-                                node[i + 2].as<VertexLabel>());
-    }
-    path_pattern.emplace_back(node[node.size() - 1].as<VertexLabel>(),
-                              MAX_EDGE_LABEL, MAX_VERTEX_LABEL);
-    return true;
-  }
-};
 
 template <>
 struct convert<VertexAttributeSegment> {
