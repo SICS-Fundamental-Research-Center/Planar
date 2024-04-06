25961753f24e3d7e502aa0ae7f875a3e2ebaf1ac
liuyang <1844061236@buaa.edu.cn>
2024-04-06 19:42:52 +0800

    del log

--- nvme/apps/wcc_nvme_app.h
+++ nvme/apps/wcc_nvme_app.h
@@ -72,24 +72,11 @@ class WCCNvmeApp : public apis::BlockModel<BlockGraph::VertexData> {
   // delete pointer 'this' in anonymous namespace
   void Compute() override {
     LOG_INFO("WCCNvmeApp::Compute() begin");
-
-    //    update_store_->LogVertexData();
-    //    MapVertex(&init);
-    //    update_store_->LogVertexData();
     int round = 0;
     while (true) {
-      //      update_store_->LogVertexData();
       MapEdge(&graft);
-      //      update_store_->LogVertexData();
-
       MapVertex(&point_jump);
-      //      update_store_->LogVertexData();
-
-      //      update_store_->LogEdgeDelInfo();
       MapAndMutateEdgeBool(&contractEdge);
-      //      update_store_->LogVertexData();
-      //      update_store_->LogEdgeDelInfo();
-
       if (update_store_.GetLeftEdges() == 0) {
         LOGF_INFO("======= Round {} end, no edges left =======", round);
         break;
