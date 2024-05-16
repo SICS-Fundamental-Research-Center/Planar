367a7313a79f47fea216c28be00ddf28758842b8
liuyang <1844061236@buaa.edu.cn>
2024-05-16 21:10:35 +0800

    add test

---
+++ core/apps/test.h
@@ -0,0 +1,64 @@
+#ifndef GRAPH_SYSTEMS_CORE_APPS_TEST_H_
+#define GRAPH_SYSTEMS_CORE_APPS_TEST_H_
+
+#include "apis/planar_app_base.h"
+#include "common/bitmap.h"
+#include "common/types.h"
+#include "data_structures/graph/mutable_csr_graph.h"
+
+namespace sics::graph::core::apps {
+
+using CSRGraph =
+    data_structures::graph::MutableCSRGraph<float,
+                                            core::common::DefaultEdgeDataType>;
+
+class TestApp : public apis::PlanarAppBase<CSRGraph> {
+  using VertexIndex = common::VertexIndex;
+  using EdgeIndex = common::EdgeIndex;
+  using VertexID = common::VertexID;
+  using VertexDegree = common::VertexDegree;
+
+ public:
+  using VertexData = CSRGraph::VertexData;
+  using EdgeData = CSRGraph::EdgeData;
+
+  TestApp() = default;
+  explicit TestApp(
+      common::TaskRunner* runner,
+      update_stores::BspUpdateStore<VertexData, EdgeData>* update_store,
+      data_structures::Serializable* graph)
+      : apis::PlanarAppBase<CSRGraph>(runner, update_store, graph) {}
+  ~TestApp() override = default;
+
+  void AppInit(common::TaskRunner* runner,
+               update_stores::BspUpdateStore<VertexData, EdgeData>*
+                   update_store) override {
+    apis::PlanarAppBase<CSRGraph>::AppInit(runner, update_store);
+  }
+
+  void PEval() {
+    LOG_INFO("TestApp PEval begins!\n");
+
+    for (VertexID id = 0; id < 100; id++) {
+      std::string info = "VertexID: " + std::to_string(id) + " ";
+      auto degree = graph_->GetOutDegreeByID(id);
+      info += "Degree: " + std::to_string(degree) + " Edges:";
+      auto edges = graph_->GetOutEdgesByID(id);
+      for (int i = 0; i < degree; i++) {
+        info += std::to_string(edges[i]) + " ";
+      }
+      update_store_->UnsetActive();
+      LOG_INFO(info.c_str());
+    }
+
+    LOG_INFO("TestApp PEval finished!\n");
+  }
+  void IncEval() {}
+  void Assemble() {}
+
+ private:
+};
+
+}  // namespace sics::graph::core::apps
+
+#endif  // GRAPH_SYSTEMS_CORE_APPS_TEST_H_
---
+++ planar/test.cpp
@@ -0,0 +1,40 @@
+#include <gflags/gflags.h>
+
+#include "core/apps/test.h"
+#include "core/planar_system.h"
+
+DEFINE_string(i, "/testfile", "graph files root path");
+DEFINE_uint32(p, 1, "parallelism");
+DEFINE_uint32(task_package_factor, 50, "task package factor");
+DEFINE_bool(in_memory, true, "in memory mode");
+DEFINE_uint32(memory_size, 64, "memory size (GB)");
+DEFINE_uint32(limits, 0, "subgrah limits for pre read");
+DEFINE_bool(no_short_cut, false, "no short cut");
+DEFINE_uint32(iter, 10, "iteration");
+
+using namespace sics::graph;
+
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  core::common::Configurations::GetMutable()->root_path = FLAGS_i;
+  core::common::Configurations::GetMutable()->parallelism = FLAGS_p;
+  core::common::Configurations::GetMutable()->in_memory = FLAGS_in_memory;
+  core::common::Configurations::GetMutable()->task_package_factor =
+      FLAGS_task_package_factor;
+  core::common::Configurations::GetMutable()->limits = FLAGS_limits;
+  core::common::Configurations::GetMutable()->short_cut = !FLAGS_no_short_cut;
+  core::common::Configurations::GetMutable()->vertex_data_size = 4;
+  core::common::Configurations::GetMutable()->vertex_data_type =
+      core::common::VertexDataType::kVertexDataTypeFloat;
+  core::common::Configurations::GetMutable()->application =
+      core::common::ApplicationType::PageRank;
+  core::common::Configurations::GetMutable()->memory_size =
+      FLAGS_memory_size * 1024;
+  core::common::Configurations::GetMutable()->pr_iter = FLAGS_iter;
+
+  LOG_INFO("System begin");
+  core::planar_system::Planar<core::apps::TestApp> system(
+      core::common::Configurations::Get()->root_path);
+  system.Start();
+  return 0;
+}
