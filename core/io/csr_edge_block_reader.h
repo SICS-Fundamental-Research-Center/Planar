967459d12e1510653271262230d651f56d11785e
liuyang <1844061236@buaa.edu.cn>
2024-06-04 16:27:25 +0800

    add io_uring reader

--- core/io/csr_edge_block_reader.h
+++ core/io/csr_edge_block_reader.h
@@ -1,6 +1,10 @@
 #ifndef GRAPH_SYSTEMS_CORE_IO_CSR_EDGE_BLOCK_READER_H_
 #define GRAPH_SYSTEMS_CORE_IO_CSR_EDGE_BLOCK_READER_H_
 
+#include <fcntl.h>
+#include <liburing.h>
+#include <sys/ioctl.h>
+
 #include <filesystem>
 #include <fstream>
 #include <list>
@@ -16,7 +20,16 @@
 #include "data_structures/serialized.h"
 #include "io/reader_writer.h"
 
+#define QD 8
+
 namespace sics::graph::core::io {
+
+struct io_data {
+  common::GraphID gid;
+  common::BlockID block_id;
+  size_t size;
+  uint32_t* addr;
+};
 
 // io_uring to read
 class CSREdgeBlockReader {
@@ -26,13 +39,90 @@ class CSREdgeBlockReader {
 
  public:
   CSREdgeBlockReader() = default;
-  CSREdgeBlockReader(const std::string& root_path) : root_path_(root_path) {}
-  void Init(const std::string& root_path) {
+  CSREdgeBlockReader(const std::string& root_path) : root_path_(root_path) {
+    auto ret = io_uring_queue_init(QD, &ring_, 0);
+    if (ret < 0) {
+      LOGF_FATAL("queue_init: {}", ret);
+    }
+  }
+  void Init(const std::string& root_path,
+            data_structures::TwoDMetadata& metadata) {
     // copy the root path
     root_path_ = root_path;
+    auto ret = io_uring_queue_init(QD, &ring_, 0);
+    if (ret < 0) {
+      LOGF_FATAL("queue_init: {}", ret);
+    }
+    blocks_addr_.resize(metadata.num_blocks_);
+    for (uint32_t i = 0; i < metadata.num_blocks_; i++) {
+      auto num = metadata.blocks_.at(i).num_sub_blocks_;
+      blocks_addr_.at(i).resize(num, nullptr);
+    }
   }
 
-  void Read(std::vector<common::BlockID> blocks_to_read) {}
+  static int get_file_size(int fd, off_t* size) {
+    struct stat st;
+    if (fstat(fd, &st) < 0) return -1;
+    if (S_ISREG(st.st_mode)) {
+      *size = st.st_size;
+      return 0;
+    } else if (S_ISBLK(st.st_mode)) {
+      unsigned long long bytes;
+      if (ioctl(fd, BLKGETSIZE64, &bytes) != 0) return -1;
+      *size = bytes;
+      return 0;
+    }
+
+    return -1;
+  }
+
+  void Read(common::GraphID gid, std::vector<common::BlockID> blocks_to_read) {
+    for (int i = 0; i < blocks_to_read.size(); i++) {
+      auto path = root_path_ + "/graphs/" + std::to_string(gid) + "_blocks" +
+                  std::to_string(blocks_to_read.at(i)) + ".bin";
+      auto fd = open(path.c_str(), O_RDONLY);
+      struct stat sb;
+      if (fstat(fd, &sb) < 0) {
+        LOG_FATAL("Error at fstat");
+      }
+      io_data* data = (io_data*)malloc(sb.st_size + sizeof(io_data));
+      data->size = sb.st_size;
+      data->gid = gid;
+      data->block_id = blocks_to_read.at(i);
+      data->addr = (uint32_t*)data + 1;
+      struct io_uring_sqe* sqe;
+      sqe = io_uring_get_sqe(&ring_);
+      if (!sqe) {
+        free(data);
+        LOG_FATAL("Error at get sqe");
+      }
+      io_uring_prep_read(sqe, fd, data->addr, data->size, 0);
+      io_uring_sqe_set_data(sqe, data);
+    }
+    auto ret = io_uring_submit(&ring_);
+    if (ret < 0) {
+      LOG_FATAL("Error at submit sqes");
+    }
+  }
+
+  std::vector<common::BlockID> GetBlockReady() {
+    struct io_uring_cqe* cqe;
+    std::vector<common::BlockID> block_ready;
+    while (true) {
+      auto ret = io_uring_peek_cqe(&ring_, &cqe);
+      if (ret != 0) {
+        break;
+      }
+      io_data* data = (io_data*)io_uring_cqe_get_data(cqe);
+      if (cqe->res != data->size * 4) {
+        LOG_FATAL("Read error in edge block, size is not fit!");
+      }
+      block_ready.push_back(data->block_id);
+      // address
+      blocks_addr_.at(data->gid).at(data->block_id) = data->addr;
+    }
+    return block_ready;
+  }
 
   size_t SizeOfReadNow() { return read_size_; }
 
@@ -40,7 +130,10 @@ class CSREdgeBlockReader {
   std::vector<common::BlockID> GetReadBlocks() {}
 
  private:
+  struct io_uring ring_;
   std::string root_path_;
+
+  std::vector<std::vector<common::VertexID*>> blocks_addr_;
   size_t read_size_ = 0;  // use MB
 };
 
